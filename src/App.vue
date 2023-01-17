f0c5fe69d36f463452a4ec8fd21c278ef92b005d
NENTI NURNANINGSIH <nentinur300901@gmail.com>
2023-01-17 21:46:16 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <h2>{{ halo }}</h2>
+  <button @click="changeName">{{bahasa}}</button>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      halo: 'Halo, Selamat datang di Website Nenti',
+      bahasa: 'English'
+    }   
+  },
+  methods: {
+    changeName() {
+      this.halo = "Hi, Welcome to Nenti's website",
+      this.bahasa = 'Bahasa Indonesia'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
